664fbdf775c3ae4e6dff8bb5916e3a0228049a8b
diegobq <diegobq@gmail.com>
2025-11-01 17:17:16 +0100

    init

---
+++ packages/server/src/load-data.ts
@@ -0,0 +1,12 @@
+import * as yaml from 'yaml'
+import * as fs from 'fs'
+import path from 'path'
+import type { SeedPacketCollectionModel } from '@seeds/models'
+
+const DATA_FILE_PATH = path.join(process.cwd(), 'data', 'seeds.yml')
+
+const data = fs.readFileSync(DATA_FILE_PATH, 'utf8')
+
+export function loadData(): SeedPacketCollectionModel {
+	return yaml.parse(data) as SeedPacketCollectionModel
+}
